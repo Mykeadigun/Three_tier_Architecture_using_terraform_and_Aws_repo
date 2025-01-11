569cc10b275dbbb7595731cec30e258923879406
Mykeadigun <mykeadigun@gmail.com>
2025-01-11 01:37:32 -0500

    bug fixes

--- aws_load_balancer.tf
+++ aws_load_balancer.tf
@@ -1,5 +1,5 @@
 # Creating External LoadBalancer
-resource "aws_lb" "external-alb" {
+resource "aws_lb" "external_alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.web_server.id]
@@ -11,7 +11,7 @@ resource "aws_lb_target_group" "target_elb" {
   vpc_id   = aws_vpc.main.id
 }
 resource "aws_lb_target_group_attachment" "attachment1" {
-  target_group_arn = "${aws_lb_target_group.external_alb.arn}"
+  target_group_arn = aws_lb_target_group.target_elb.arn
   target_id        = aws_instance.web_instance1.id
   port             = 80
 depends_on = [
@@ -19,19 +19,19 @@ depends_on = [
   ]
 }
 resource "aws_lb_target_group_attachment" "attachment2" {
-  target_group_arn = "${aws_lb_target_group.external-alb.arn}"
+  target_group_arn = aws_lb_target_group.target_elb.arn
   target_id        = aws_instance.web_instance2.id
   port             = 80
 depends_on = [
     aws_instance.web_instance2,
   ]
 }
-resource "aws_lb_listener" "external-elb" {
-  load_balancer_arn = aws_lb.external-alb.arn
+resource "aws_lb_listener" "external_elb" {
+  load_balancer_arn = aws_lb.external_alb.arn
   port              = "80"
   protocol          = "HTTP"
 default_action {
     type             = "forward"
-    target_group_arn = "${aws_lb_target_group.external-alb.arn}"
+    target_group_arn = aws_lb_target_group.target_elb.arn
   }
 }
--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web_instance1" {
   ami                     = data.aws_ssm_parameter.instance_ami.value
   instance_type           = "${var.instance_type}"
   key_name                = "${var.keypair}"
-  vpc_security_group_ids  = [aws.security_group.web_server.id]
+  vpc_security_group_ids  = [aws_security_group.web_server.id]
   subnet_id               = aws_subnet.websubnet1.id
   associate_public_ip_address = true
   user_data = file("./data.sh")
@@ -12,7 +12,7 @@ resource "aws_instance" "web_instance2" {
   ami                     = data.aws_ssm_parameter.instance_ami.value
   instance_type           = "${var.instance_type}"
   key_name                = "${var.keypair}"
-  vpc_security_group_ids  = [aws.security_group.web_server.id]
+  vpc_security_group_ids  = [aws_security_group.web_server.id]
   subnet_id               = aws_subnet.websubnet2.id
   associate_public_ip_address = true
   user_data = file("./data.sh")
