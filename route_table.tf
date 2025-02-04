122c64de16397c5a6506b6e46d2e50e58f1149f0
Mykeadigun <mykeadigun@gmail.com>
2025-02-04 18:38:59 -0500

    changes to route table

--- route_table.tf
+++ route_table.tf
@@ -16,8 +16,7 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = aws_nat_gateway.nat_gw.id
+    cidr_block = var.vpc_cidr
   }
 
   tags = {
